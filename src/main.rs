43c9294986278fb3e72f56958abb5c4ed3fdf52a
wardliu <wardliu@apjcorp.com>
2024-05-16 17:57:16 +0800

    update smart pointer code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-// use rust_study::smart_pointer;
 mod smart_pointer_15;
 
 fn main() {
-    // println!("Hello, world!");
     smart_pointer_15::example();
 }
--- src/smart_pointer_15/mod.rs
+++ src/smart_pointer_15/mod.rs
@@ -1,81 +1,65 @@
-#[cfg(test)]
-mod tests {
-    use std::cell::RefCell;
+use std::{
+    cell::RefCell,
+    rc::{Rc, Weak},
+};
 
-    use super::*;
+mod messenger;
+mod tree_data;
 
-    struct MockMessenger {
-        sent_messages: RefCell<Vec<String>>,
-    }
-
-    impl MockMessenger {
-        fn new() -> MockMessenger {
-            MockMessenger {
-                sent_messages: RefCell::new(vec![]),
-            }
-        }
-    }
-
-    impl Messenger for MockMessenger {
-        fn send(&self, message: &str) {
-            // self.sent_messages.borrow_mut().push(String::from(message));
-            let mut one_borrow = self.sent_messages.borrow_mut();
-            let mut two_borrow = self.sent_messages.borrow_mut();
-
-            one_borrow.push(String::from(message));
-            two_borrow.push(String::from(message))
-        }
-    }
-
-    #[test]
-    fn it_sends_an_over_75_percent_warning_message() {
-        let mock_messenger = MockMessenger::new();
-        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
-
-        limit_tracker.set_value(80);
-
-        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
-    }
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
 }
 
-pub trait Messenger {
-    fn send(&self, msg: &str);
-}
-
-pub struct LimitTracker<'a, T: Messenger> {
-    messenger: &'a T,
-    value: usize,
-    max: usize,
-}
-
-impl<'a, T> LimitTracker<'a, T>
-where
-    T: Messenger,
-{
-    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
-        LimitTracker {
-            messenger,
-            value: 0,
-            max,
-        }
-    }
-    pub fn set_value(&mut self, value: usize) {
-        self.value = value;
-
-        let percentage_of_max = self.value as f64 / self.max as f64;
-
-        if percentage_of_max >= 1.0 {
-            self.messenger.send("Error: You are over your quota!");
-        } else if percentage_of_max >= 0.9 {
-            self.messenger
-                .send("Urgent warning: You've used up over 90% of your quota!");
-        } else if percentage_of_max >= 0.75 {
-            self.messenger
-                .send("Warning: You've used up over 75% of your quota!")
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
         }
     }
 }
+
+use crate::smart_pointer_15::List::{Cons, Nil};
+
+use self::tree_data::Node;
 
 pub fn example() {
-    //
+    // let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    // println!("a initial rc count = {}", Rc::strong_count(&a));
+    // println!("a next item {:?}", a.tail());
+
+    // let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    // println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    // println!("b initial rc count = {}", Rc::strong_count(&b));
+    // println!("b next item {:?}", b.tail());
+
+    // if let Some(link) = a.tail() {
+    //     *link.borrow_mut() = Rc::clone(&b);
+    // }
+
+    // println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    // println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // // println!("a next item = {:?}", a.tail());
+    let leaf = Rc::new(tree_data::Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(tree_data::Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
